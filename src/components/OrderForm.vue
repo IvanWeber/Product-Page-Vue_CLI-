f16c19194d7284a388923f24f0508ca753fb79aa
Ivan Weber <b24b24testing.ru>
2019-11-25 02:57:24 +0300

    Реализована валидация формы заказа звонка

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -4,11 +4,11 @@
         <form action="php/send.php" method="post">
             <div class="form-group">
                 <label for="phone">Телефон</label>
-                <input type="tel" name="phone" class="form-control" id="phone" aria-describedby="emailHelp" placeholder="Ваш телефон">
+                <input type="tel" name="phone" class="form-control" id="phone" aria-describedby="emailHelp" placeholder="Ваш телефон в формате +х-ххх-ххх-хх-хх" pattern="+^\d{1}-\d{3}-\d{3}$-\d{2}$-\d{2}$" required>
             </div>
             <div class="form-group">
                 <label for="name">Имя</label>
-                <input type="text" name="name" class="form-control" id="name" placeholder="Ваше имя">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Ваше имя" required>
             </div>
             <button type="submit" class="btn btn-primary">Отправить</button>
         </form>
