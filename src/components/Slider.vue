b2d8c5f6b7540b153832cc4fef0fe812c850bc4b
Ivan Weber <b24b24testing.ru>
2019-11-25 01:19:50 +0300

    сделан слайдер

---
+++ src/components/Slider.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="col-md-5">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="../assets/lenovo-1.webp" alt="Первый слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="../assets/lenovo-2.webp" alt="Второй слайд">
+                </div>
+                <div class="carousel-item" style="vertical-align: middle;">
+                    <img class="d-block w-100" src="../assets/lenovo-3.webp" alt="Третий слайд">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Slider"
+    }
+</script>
+
+<style scoped>
+    .carousel-indicators li {
+        filter: invert(50%);
+    }
+
+    .carousel-control-next,
+    .carousel-control-prev {
+        filter: invert(50%);
+    }
+
+    .carousel-inner {
+        height: 500px;
+        display:flex;
+        align-items: center;
+    }
+</style>
